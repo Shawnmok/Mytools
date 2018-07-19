232668e6b77a4dd92dc59b1da79cb386bf5bee7a
Shawn MOK <37991886+Shawnmok@users.noreply.github.com>
2018-07-19 09:27:26 +0800

    Update scatter_viollin_depict_plot.py

--- scatter_viollin_depict_plot.py
+++ scatter_viollin_depict_plot.py
@@ -6,9 +6,9 @@ import seaborn as sns
 import  numpy as np
 import pandas as pd
 
-tips = pd.read_csv("./t3.2.csv")
+tips = pd.read_csv("./IMPUT_FILE.csv")
 print (tips)
-name="age,BMI,WHR,FBS,2hPBS,GADA_index,height,weight,waist,hip,TG,TC,HDL,LDL,HbA1c,FCP,2hCP,GADA,onset_age".split(",")
+name="Feature1,Feature2,Feature3,Feature4,Feature5,Feature6,Feature7,Feature8,Feature9,Feature10,Feature11,Feature12,Feature13,Feature14,Feature15,Feature16,Feature17,Feature18,Feature19".split(",")
 plt.subplots(3,3,figsize=(20,15))
 for i in range(1,10):
    print(name[i])
